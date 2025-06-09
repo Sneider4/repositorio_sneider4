27b89eeac6c5fde269111747377072c44622d408
Sneider4 <richard_malagon@soy.sena.edu.co>
2025-06-09 10:21:05 -0500

    [+] Realizo cambio en la interfaz, aun faltan mejoras.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./compartido/navbar/navbar.component";
-import { FooterComponent } from "./compartido/footer/footer.component";
+import { NavbarComponent } from './compartido/navbar/navbar.component';
+import { FooterComponent } from './compartido/footer/footer.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, NgClass],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'portafolio-richard';
+
+  isSidebarCollapsed = false;
+
+  onSidebarToggle(collapsed: boolean) {
+    this.isSidebarCollapsed = collapsed;
+  }
 }
